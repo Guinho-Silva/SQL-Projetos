3a335bcfdd02bcf2ec1a955309a1bbcf4d07e796
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-01-20 20:09:04 -0300

    Modifica o arquivo DML

--- mini-projeto-sql/dml.sql
+++ mini-projeto-sql/dml.sql
@@ -1,16 +1,16 @@
 
 -- Inserção dos funcionários
 
-INSERT INTO `tabelafuncionario` (`Id_Funcionario`, `Nome_funcionario`, `Departamento`, `Salario_funcionario`) VALUES
-(1, 'Heitor Vieira', 'Financeiro', 4959.22),
-(3, 'Luiza Dias', 'TI', 7500.00),
-(4, 'Davi Lucas Moraes', 'Financeiro', 8437.02),
-(5, 'Pietro Cavalcanti', 'TI', 7500.00),
-(6, 'Evelyn da Mata', 'Vendas', 5278.88),
-(7, 'Isabella Rocha', 'Marketing', 4006.03),
-(8, 'Sra. Manuela Azevedo', 'Vendas', 6101.88),
-(9, 'Brenda Cardoso', 'TI', 7500.00),
-(10, 'Danilo Souza', 'TI', 7500.00);
+INSERT INTO `tabelafuncionario` (`Nome_funcionario`, `Departamento`, `Salario_funcionario`) VALUES
+('Heitor Vieira', 'Financeiro', 4959.22),
+('Luiza Dias', 'TI', 7500.00),
+('Davi Lucas Moraes', 'Financeiro', 8437.02),
+('Pietro Cavalcanti', 'TI', 7500.00),
+('Evelyn da Mata', 'Vendas', 5278.88),
+('Isabella Rocha', 'Marketing', 4006.03),
+('Sra. Manuela Azevedo', 'Vendas', 6101.88),
+('Brenda Cardoso', 'TI', 7500.00),
+('Danilo Souza', 'TI', 7500.00);
 
 -- Atualiza o salário dos funcionários do TI para 7500
 
@@ -24,13 +24,12 @@ WHERE Salario_funcionario < 4000;
 
 -- Inserção dos projetos
 INSERT INTO TabelaProjetos (
-    Id_Projeto,
     Nome_Projeto,
     fk_Funcionario
 )
 VALUES
-(1, 'Projeto-Minha-Casa', 4),
-(2, 'Projeto-Automação_de_IA', 3),
-(3, 'Projeto-Agentes_de_IA', 4);
+('Projeto-Minha-Casa', 4),
+('Projeto-Automação_de_IA', 3),
+('Projeto-Agentes_de_IA', 4);
 
 
