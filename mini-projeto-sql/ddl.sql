918d4235af8baa717f2dd648871633c6ca378d37
Iago Silva <208397471+Guinho-Silva@users.noreply.github.com>
2026-01-20 20:01:28 -0300

    Atualiza o arquivo DDL

--- mini-projeto-sql/ddl.sql
+++ mini-projeto-sql/ddl.sql
@@ -5,15 +5,18 @@ USE Mini_Projeto;
 
 
 CREATE TABLE `tabelafuncionario` (
-  `Id_Funcionario` int(11) NOT NULL,
-  `Nome_funcionario` varchar(100) DEFAULT NULL,
-  `Departamento` varchar(100) DEFAULT NULL,
-  `Salario_funcionario` decimal(10,2) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Id_Funcionario` int PRIMARY KEY AUTOINCREMENT,
+  `Nome_funcionario` varchar(100),
+  `Departamento` varchar(100),
+  `Salario_funcionario` decimal(10,2)
+);
 
 
 CREATE TABLE `tabelaprojetos` (
-  `Id_Projeto` int(11) NOT NULL,
-  `Nome_Projeto` varchar(100) DEFAULT NULL,
-  `fk_Funcionario` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Id_Projeto` int PRIMARY KEY AUTOINCREMENT,
+  `Nome_Projeto` varchar(100),
+  `fk_Funcionario` int,
+  CONSTRAINT fk_funcionario
+    FOREIGN KEY(fk_Funcionario)
+    REFERENCES tabelafuncionario(Id_Funcionario)
+);
